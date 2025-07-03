64516c72159b28e3cc1f90ad558bdb73a7ccf954
xDAGAx13 <rohandaga05@gmail.com>
2025-07-03 23:41:48 +0530

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <h1 className='text-2xl text-white underline'></h1>
+    </main>
+  )
+}
+
+export default App
