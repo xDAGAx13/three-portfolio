355c0c8a322dc8228b1a5dcb3611e0b13fe2aa23
xDAGAx13 <rohandaga05@gmail.com>
2025-07-04 16:49:51 +0530

    insert 3d object

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './sections/Navbar';
+import Hero from './sections/Hero';
 
 const App = () => (
   <main className="max-w-7xl mx-auto">
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { PerspectiveCamera } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import HackerRoom from '../components/HackerRoom'
+import CanvasLoader from '../components/CanvasLoader'
 
 const Hero = () => {
   return (
-    <section className='min-h-screen w-full flex-col relative'>
+    <section className='min-h-screen w-full flex flex-col relative'>
       <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
         <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Rohan</p>
         <p className='hero_tag text-gray_gradient'>Building Products & Brands</p>
+
+        <div className='w-full h-full absolute inset-0'>
+          <Canvas className='w-full h-full'>
+            <Suspense fallback={<CanvasLoader/>}>
+
+            <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+            <HackerRoom scale={0.05} position={[0,0,0]} rotation={[0, 0, 0]}/>  
+
+            <ambientLight intensity={1}/>
+            <directionalLight position={[10, 10, 10]} intensity={0.5}/>
+            </Suspense>
+          </Canvas>
+        </div>
       </div>
     </section>
   )
